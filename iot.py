a98391c11513835143051d94441bb2d288d6c4f4
Samuel Ellsworth <s.e@c-a-t.cloud>
2019-10-05 04:29:09 +0000

    initial commit

---
+++ iot.py
@@ -0,0 +1,22 @@
+import time
+from machine import Pin
+
+p32 = Pin(32, Pin.IN)
+p33 = Pin(33, Pin.OUT)
+p33.value(1)
+
+def wait_pin_change(pin):
+    counter = 0
+    while counter < 2:
+        if pin.value() != 0:
+            counter += 1
+        else:
+            counter = 0
+        time.sleep(1)
+
+while True:
+    wait_pin_change(p32)
+    print(p32.value())
+
+# response = urequests.post("http://192.168.200.103/iot", data = "test mesage")
+   # print(response.json())
